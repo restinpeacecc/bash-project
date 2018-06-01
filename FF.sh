4fd6863b0dc5484fbad6bc0fe79dd59e3fad7c4d
陳　誠 <restinpeace@restinpeace-MBA.local>
2018-06-02 03:28:37 +0900

    Update 5

--- FF.sh
+++ FF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Create by R.I.P_C on 2018/06/01
-echo -e "\033[34mFile Finder v1.1.5 by R.I.P_C\033[0m"
+echo -e "\033[32mFile Finder v1.1.5 by R.I.P_C\033[0m"
 echo -e "\033[31m$LOGNAME\033[0m on `date`"
 echo
 
